c5153ff666650a5043929283e591d6fe4fccc9ae
Dmitrii Mitin <mda_200418@mail.ru>
2025-12-30 23:41:31 -0500

    make server port configurable for deployment

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -21,6 +21,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ? Number(process.env.PORT) : 3000
+  await app.listen(port)
 }
 bootstrap();
