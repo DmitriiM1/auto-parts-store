d6d92873a7a4581fb2ac0ef0585199e924a50eed
Dmitrii Mitin <mda_200418@mail.ru>
2026-01-04 21:25:51 -0500

    add swagger servers for prod and local

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe(
     {
       whitelist: true,
@@ -12,16 +17,22 @@ async function bootstrap() {
     }
   ));
 
+  const port = process.env.PORT ? Number(process.env.PORT) : 3000
+
+
+  const prodUrl = process.env.PUBLIC_URL || 'https://auto-parts-store-fa9b.onrender.com'
+
   const config = new DocumentBuilder()
     .setTitle('Auto Parts Store API')
     .setDescription('Backend API for Auto Parts Store')
     .setVersion('1.0.0')
-    .build();
+    .addServer(prodUrl, 'Production (Render)')
+    .addServer(`http://localhost:${port}`, 'Local')
+    .build()
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('docs', app, document)
 
-  const port = process.env.PORT ? Number(process.env.PORT) : 3000
   await app.listen(port)
 }
 bootstrap();
